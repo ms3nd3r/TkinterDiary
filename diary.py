3321f6e420c14cdc67a20aa9592e1954c811bee9
ms3nd3r <casm1200160@gn.iwasaki.ac.jp>
2023-05-01 10:24:19 +0900

    messageboxの不具合修正とreadmeの追記

--- diary.py
+++ diary.py
@@ -1,6 +1,11 @@
+import os
 import tkinter as tk
+from tkinter import messagebox
 from datetime import datetime
 
+# diaryDataディレクトリがない場合は作成する
+if not os.path.exists('diaryData'):
+    os.makedirs('diaryData')
 
 # メインウィンドウの作成
 root = tk.Tk()
@@ -15,13 +20,13 @@ text.pack()
 def save_text():
     diary = text.get('1.0', 'end-1c')  # 入力されたテキストの取得
     today = datetime.today().strftime('%Y年%m月')
-    file_name = today + '.txt'
+    file_name = os.path.join('diaryData', today + '.txt')  # ファイルのパスを指定する
     with open(file_name, 'a', encoding='utf-8') as f:
         timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')  # タイムスタンプの取得
         f.write(timestamp + '\n')  # タイムスタンプの書き込み
         f.write(diary + '\n\n')  # ファイルにテキストを書き込む
     text.delete('1.0', 'end')  # テキストエリアの内容を初期化する
-    tk.messagebox.showinfo('更新完了！', '日記を更新しました。')  # アラートメッセージの表示
+    messagebox.showinfo('更新完了！', '日記を更新しました。')  # アラートメッセージの表示
 
 # 保存ボタンの作成
 save_button = tk.Button(root, text='保存', command=save_text)
